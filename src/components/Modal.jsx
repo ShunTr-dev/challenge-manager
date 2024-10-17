da3b4b24827c9f35c6c947fa74950665d5d61c9d
ShunTr <repaco9@gmail.com>
2024-10-17 21:12:52 +0200

    Reusing states

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,12 +8,16 @@ export default function Modal({ title, children, onClose }) {
             <motion.dialog
                 open
                 className="modal"
-                initial={{ opacity: 0, y: 30 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 1, y: 30 }}>
+                variants={{
+                    hidden: { opacity: 0, y: 30 },
+                    visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                animate="visible"
+                exit="hidden">
                 <h2>{title}</h2>
                 {children}
-            </motion.dialog>
+            </motion.dialog>q
         </>,
         document.getElementById('modal')
     )
